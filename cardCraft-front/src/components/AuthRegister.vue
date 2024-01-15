d182dbff5822f29671a075bcb147d77464b93ed8
blupblupdoux <sarah.melin19403@gmail.com>
2024-01-15 15:04:42 +0100

    DEV-14 color theme + register page

--- cardCraft-front/src/components/AuthRegister.vue
+++ cardCraft-front/src/components/AuthRegister.vue
@@ -1,12 +1,59 @@
 <template>
-    <div></div>
+    <div id="registerPage">
+      <h1>Create an account</h1>
+      <q-form>
+        <small>Only field with a * are required.</small>
+        <q-input v-model="form.name" label="Name" type="text" outlined></q-input>
+        <q-input v-model="form.email" label="Email" type="email" outlined></q-input>
+        <q-input v-model="form.username" label="Username*" type="text" outlined></q-input>
+        <q-input v-model="form.password" label="Password*" type="password" outlined></q-input>
+        <q-input v-model="form.password_confirm" label="Password confirmation*" type="password" outlined></q-input>
+
+        <q-btn color="primary" label="Submit" />
+      </q-form>
+    </div>
 </template>
 
 <script setup>
+import { ref, reactive } from 'vue';
 import { api } from 'src/boot/axios'
+
+let form = reactive({
+  name: '',
+  email: '',
+  username: '',
+  password: '',
+  password_confirm: ''
+})
 
 // const register = () => {
 //   // api.post('/api/register', {username: 'test1234', password: 'test123', name: 'test123', email: 'test1234@test.com'})
 // }
 
 </script>
+
+<style lang=scss scoped>
+
+#registerPage {
+  padding: 0 1rem;
+
+  h1 {
+    text-align: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    small, .q-field {
+      margin-bottom: 1rem;
+    }
+
+    .q-btn {
+      align-self: center;
+      margin-top: .5rem;
+    }
+  }
+}
+
+</style>
--- cardCraft-front/src/router/routes.js
+++ cardCraft-front/src/router/routes.js
@@ -8,6 +8,10 @@ const routes = [
     path: '/login',
     component: () => import('components/AuthLogin.vue'),
   },
+  {
+    path: '/register',
+    component: () => import('components/AuthRegister.vue'),
+  },
 
   // Always leave this as last one,
   // but you can also remove it
