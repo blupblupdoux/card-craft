a6c908686ad2146de14ad67ee1c35202776bc26a
blupblupdoux <sarah.melin19403@gmail.com>
2024-01-15 11:56:42 +0100

    DEV-14 update AuthController to use API auth

--- cardCraft-api/app/Http/Controllers/AuthController.php
+++ cardCraft-api/app/Http/Controllers/AuthController.php
@@ -2,26 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    public function register(Request $request) {
+
+        // Check data
+        $data = $request->validate([
+            'username' => 'string|required|unique:users',
+            'password' => 'required',
+            'name' => 'string|nullable',
+            'email' => 'email|nullable|unique:users',
+        ]);
+
+        // Create new user
+        $newUser = User::create([
+            'username' => $data['username'],
+            'password' => Hash::make($data['password']),
+            'name' => $data['name'],
+            'email' => $data['email']
+        ]);
+
+        return (new JsonResource([
+            'message' => 'Successful registeration.',
+            'user' => $newUser,
+            'token' => $newUser->createToken(uniqid())->plainTextToken
+        ]))->response()->setStatusCode(Response::HTTP_OK);
+    }
+
     public function authenticate(Request $request) {
 
         // Check data
         $credentials = $request->validate([
-            'username' => ['required'],
-            'password' => ['required']
+            'username' => 'string|required',
+            'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return (new JsonResource(['message'=> 'Successful authentication.']))->response()->setStatusCode(Response::HTTP_OK);
+        // Check credentials
+        if(!Auth::attempt($credentials)) {
+            return (new JsonResource(['message'=> 'Wrong credentials.']))->response()->setStatusCode(Response::HTTP_UNAUTHORIZED);
         }
 
-        return (new JsonResource(['message'=> 'Wrong credentials.']))->response()->setStatusCode(Response::HTTP_UNAUTHORIZED);
+        $user = User::where('email', $request->email)->first();
+
+        return (new JsonResource([
+            'message' => 'Successful authentication.',
+            'user' => $user,
+            'token' => $user->createToken(uniqid())->plainTextToken
+        ]))->response()->setStatusCode(Response::HTTP_OK);
     }
 }
--- cardCraft-api/routes/api.php
+++ cardCraft-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/testRoute', function (Request $request) {
     return '';
 });
+
+// Authentication
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/authenticate', [AuthController::class, 'authenticate']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
