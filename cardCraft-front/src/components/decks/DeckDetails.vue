372cf483859868ab73085d5369fb1975d55670ef
blupblupdoux <sarah.melin19403@gmail.com>
2024-02-02 10:10:45 +0100

    DEV-19 background design

--- cardCraft-front/src/components/decks/DeckDetails.vue
+++ cardCraft-front/src/components/decks/DeckDetails.vue
@@ -1,8 +1,21 @@
 <template>
-    <div v-if="'id' in deck">
+    <div id="deckDetails" v-if="'id' in deck">
          <div>
-            <header-default :text="'coucou'" route="/decks"></header-default>
-            <div></div>
+            <!-- Header -->
+            <div class="header-primary">
+                <div class="header-icons">
+                    <back-arrow to="/decks"></back-arrow>
+                </div>
+                <div class="header-content">
+                    <div>{{ deck.name }}</div>
+                    <div class="deck-details-description">{{ deck.description }}</div>
+                </div>
+            </div>
+
+            <!-- Cards list -->
+            <div class="deck-details-cards page-padding-default">
+
+            </div>
         </div>
         <deck-details-actions :deck="deck"></deck-details-actions>  
     </div>
@@ -12,7 +25,7 @@
 import { useI18n } from 'vue-i18n';
 import { useDecksStore } from 'src/stores/decks-store';
 import DeckDetailsActions from './DeckDetailsActions.vue';
-import HeaderDefault from '../common/HeaderDefault.vue';
+import BackArrow from '../common/BackArrow.vue';
 
 const props = defineProps({ id: String })
 const { t } = useI18n()
@@ -22,5 +35,29 @@ let deck = decksStore.getDeck(props.id)
 
 </script>
 
-<style>
+<style lang="scss">
+#deckDetails {
+    .header-primary {
+        height: 38vh;
+    }
+    .header-icons {
+        padding: .5rem .5rem 0rem .5rem;
+    }
+    .header-content {
+        padding: .5rem 2rem 2rem 2rem;
+    }
+    .deck-details-description {
+        font-size: small;
+        margin-top: 1rem;
+    }
+    .deck-details-cards{
+        height: 62vh;
+        position: relative;
+        top: -40px;
+        background-color: white;
+        border-top-left-radius: 30px;
+        border-top-right-radius: 30px;
+        box-shadow: 0px -2px 17px 0px rgba(0,0,0,0.25);
+    }
+}
 </style>
